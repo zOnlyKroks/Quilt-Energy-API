5b104a39f69f62b4ffeefd3d2709ed2dc8a11152
yoyosource <yoyosource@nidido.de>
2022-07-25 14:32:41 +0200

    Add javadoc

--- src/main/java/de/flow/api/StatefulNetworkCable.java
+++ src/main/java/de/flow/api/StatefulNetworkCable.java
@@ -4,6 +4,20 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+/**
+ * Used to change the cable appearance based on the available amount in the current tick.
+ *
+ * @param <C> generic-type of the network cable
+ */
 public interface StatefulNetworkCable<C> extends NetworkCable<C> {
+
+	/**
+	 * Change the block state of the cable based on the available amount in the current tick.
+	 *
+	 * @param world the world
+	 * @param blockPos the block position
+	 * @param blockState the current block state
+	 * @param availableAmount the available amount in the current tick
+	 */
 	void changeCableState(World world, BlockPos blockPos, BlockState blockState, C availableAmount);
 }
