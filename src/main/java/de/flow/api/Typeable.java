88799ee087f7b8f01812763a929b89458643d5c6
yoyosource <yoyosource@nidido.de>
2022-07-25 14:25:48 +0200

    Add javadoc

--- src/main/java/de/flow/api/Typeable.java
+++ src/main/java/de/flow/api/Typeable.java
@@ -1,5 +1,15 @@
 package de.flow.api;
 
+/**
+ * Typeable is used for elements of the flow api that need a type to be attached to them.
+ *
+ * @param <C> The generic-type of the {@link Type}
+ */
 public interface Typeable<C> {
+	/**
+	 * Returns the {@link Type} of the element.
+	 *
+	 * @return non-null {@link Type}
+	 */
 	Type<C> type();
 }
--- src/main/java/de/flow/api/Unitable.java
+++ src/main/java/de/flow/api/Unitable.java
@@ -1,5 +1,15 @@
 package de.flow.api;
 
+/**
+ * Unitable is used for elements of the flow api that need a unit to be attached to them.
+ *
+ * @param <C> The generic-type of the {@link Unit}
+ */
 public interface Unitable<C> {
+	/**
+	 * Returns the {@link Unit} of the element.
+	 *
+	 * @return non-null {@link Unit}
+	 */
 	Unit<C> unit();
 }
