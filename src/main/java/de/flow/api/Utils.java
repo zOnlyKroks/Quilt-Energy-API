b5b3fddb0da90d7de3890c1eb30a8fe0746a5b3a
yoyosource <yoyosource@nidido.de>
2022-07-25 14:22:09 +0200

    Add javadoc

--- src/main/java/de/flow/api/Utils.java
+++ src/main/java/de/flow/api/Utils.java
@@ -7,12 +7,24 @@ import java.math.BigInteger;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * Utility class for the Flow API.
+ */
 @UtilityClass
 public class Utils {
 
+	/**
+	 * A default type implementation used for energy.
+	 */
 	public static final Type<AtomicDouble> ENERGY_TYPE = new Type.NumberType();
 
+	/**
+	 * A default type implementation used for redstone.
+	 */
 	public static final Type<AtomicInteger> REDSTONE_TYPE = new RedstoneType();
 
+	/**
+	 * A default type implementation used for items.
+	 */
 	public static final Type<Map<ItemStackContainer, BigInteger>> ITEM_TYPE = new ItemType();
 }
