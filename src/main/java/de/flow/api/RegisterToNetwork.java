38cd4218eb19ff38a560faad19c0ad12052d26b1
yoyosource <yoyosource@nidido.de>
2022-07-25 14:35:14 +0200

    Add javadoc

--- src/main/java/de/flow/api/RegisterToNetwork.java
+++ src/main/java/de/flow/api/RegisterToNetwork.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to mark a field with type {@link de.flow.api.NetworkBlock.Input},
+ * {@link de.flow.api.NetworkBlock.Output}, {@link de.flow.api.NetworkBlock.Store}, or
+ * {@link de.flow.api.NetworkBlock.Transmitter} from a {@link NetworkBlock} to be used by a {@link Network}.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface RegisterToNetwork {
